1a1f6a2ed8ab48d6e1b379226e380f671bdfe5a7
AaronTan11 <tanweihup@gmail.com>
2024-07-27 23:59:49 +0800

    feat: added some projects ui

--- moovitz-frontend/src/app/project/page.jsx
+++ moovitz-frontend/src/app/project/page.jsx
@@ -1,7 +1,71 @@
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+
 export default function ProjectPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-3xl font-bold">Projects</h1>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center justify-center h-full'>
+			<h1 className='text-3xl font-bold'>Projects</h1>
+			<HoverEffect items={projects} />
+		</div>
+	);
 }
+export const projects = [
+	{
+		title: "Stripe",
+		description: "A technology company that builds economic infrastructure for the internet.",
+		link: "https://stripe.com",
+		map: (
+			<iframe
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15935.648316042638!2d101.67398099999998!3d3.11795795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc498f47c6e729%3A0x8bb7d7059cd7d8d4!2sKL%20Eco%20City%2C%2059200%20Kuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1722092243400!5m2!1sen!2smy'
+				className='w-full'
+				allowfullscreen='true'
+				loading='lazy'
+				referrerpolicy='no-referrer-when-downgrade'
+			></iframe>
+		),
+	},
+	{
+		title: "Netflix",
+		description:
+			"A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+		link: "https://netflix.com",
+		map: (
+			<iframe
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15935.648316042638!2d101.67398099999998!3d3.11795795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc498f47c6e729%3A0x8bb7d7059cd7d8d4!2sKL%20Eco%20City%2C%2059200%20Kuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1722092243400!5m2!1sen!2smy'
+				className='w-full'
+				allowfullscreen='true'
+				loading='lazy'
+				referrerpolicy='no-referrer-when-downgrade'
+			></iframe>
+		),
+	},
+	{
+		title: "Google",
+		description:
+			"A multinational technology company that specializes in Internet-related services and products.",
+		link: "https://google.com",
+		map: (
+			<iframe
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15935.648316042638!2d101.67398099999998!3d3.11795795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc498f47c6e729%3A0x8bb7d7059cd7d8d4!2sKL%20Eco%20City%2C%2059200%20Kuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1722092243400!5m2!1sen!2smy'
+				className='w-full'
+				allowfullscreen='true'
+				loading='lazy'
+				referrerpolicy='no-referrer-when-downgrade'
+			></iframe>
+		),
+	},
+	{
+		title: "Meta",
+		description:
+			"A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+		link: "https://meta.com",
+		map: (
+			<iframe
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15935.648316042638!2d101.67398099999998!3d3.11795795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc498f47c6e729%3A0x8bb7d7059cd7d8d4!2sKL%20Eco%20City%2C%2059200%20Kuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1722092243400!5m2!1sen!2smy'
+				className='w-full'
+				allowfullscreen='true'
+				loading='lazy'
+				referrerpolicy='no-referrer-when-downgrade'
+			></iframe>
+		),
+	},
+];
---
+++ moovitz-frontend/src/components/ui/card-hover-effect.jsx
@@ -0,0 +1,79 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalTrigger } from "../ui/animated-modal";
+
+export const HoverEffect = ({ items, className }) => {
+	let [hoveredIndex, setHoveredIndex] = useState(null);
+
+	return (
+		<div className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10", className)}>
+			{items.map((item, idx) => (
+				<>
+					<div
+						key={item?.link}
+						className='relative group  block p-2 h-full w-full'
+						onMouseEnter={() => setHoveredIndex(idx)}
+						onMouseLeave={() => setHoveredIndex(null)}
+					>
+						<Card className='flex flex-col h-full'>
+							<div className='flex-grow'>
+								<CardTitle className={"space-y-6"}>
+									{item?.map}
+									<p>{item.title}</p>
+								</CardTitle>
+								<CardDescription>{item.description}</CardDescription>
+							</div>
+							<Modal>
+								<ModalTrigger className='bg-white text-black flex justify-center group/modal-btn mt-4'>
+									<span className='group-hover/modal-btn:translate-x-40 text-center transition duration-500'>
+										CrowdFund Now!
+									</span>
+									<div className='-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20'>
+										✈️
+									</div>
+								</ModalTrigger>
+								<ModalBody className={"z-[1000000]"}>
+									<ModalContent></ModalContent>
+									<ModalFooter className='gap-4'>
+										<button className='px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28'>
+											Cancel
+										</button>
+										<button className='bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28'>
+											Book Now
+										</button>
+									</ModalFooter>
+								</ModalBody>
+							</Modal>
+						</Card>
+					</div>
+				</>
+			))}
+		</div>
+	);
+};
+
+export const Card = ({ className, children }) => {
+	return (
+		<div
+			className={cn(
+				"rounded-2xl h-full w-full p-5 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 ",
+				className
+			)}
+		>
+			{children}
+		</div>
+	);
+};
+export const CardTitle = ({ className, children }) => {
+	return <h4 className={cn("text-zinc-100 font-bold tracking-wide", className)}>{children}</h4>;
+};
+export const CardDescription = ({ className, children }) => {
+	return (
+		<p className={cn("mt-3 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>
+			{children}
+		</p>
+	);
+};
