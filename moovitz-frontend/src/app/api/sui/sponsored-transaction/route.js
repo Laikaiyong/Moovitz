b2a791c586f61571fb148972ce0b29e30acb7d5d
AaronTan11 <tanweihup@gmail.com>
2024-07-31 08:54:23 +0800

    feat: update ST

--- moovitz-frontend/src/app/api/sui/sponsored-transaction/route.js
+++ moovitz-frontend/src/app/api/sui/sponsored-transaction/route.js
@@ -1,8 +1,8 @@
-async function incrementCounter() {
+async function topUpToken(amount) {
 	const promise = async () => {
-		track("Increment Counter");
+		track("Top Up Token");
 
-		setCounterLoading(true);
+		setTopUpLoading(true);
 
 		// Get the keypair for the current user.
 		const keypair = await enokiFlow.getKeypair({ network: "testnet" });
@@ -10,13 +10,10 @@ async function incrementCounter() {
 		// Create a new transaction block
 		const txb = new Transaction();
 
-		// Add some transactions to the block...
+		// Add the top-up transaction to the block
 		txb.moveCall({
-			arguments: [
-				txb.object("0xd710735500fc1be7dc448b783ad1fb0b5fd209890a67e518cc47e7dc26856aa6"),
-			],
-			target:
-				"0x5794fff859ee70e28ec8a419f2a73830fb66bcaaaf76a68e41fcaf5e057d7bcc::global_counter::increment",
+			arguments: [txb.pure(amount), txb.object("0x1234567890abcdef1234567890abcdef")],
+			target: "0x9876543210fedcba9876543210fedcba::token::top_up",
 		});
 
 		try {
@@ -26,30 +23,12 @@ async function incrementCounter() {
 				network: "testnet",
 				client,
 			});
-			setCounterLoading(false);
+			setTopUpLoading(false);
 
 			return res;
 		} catch (error) {
-			setCounterLoading(false);
+			setTopUpLoading(false);
 			throw error;
 		}
 	};
-
-	toast.promise(promise, {
-		loading: "Incrementing counter...",
-		success: (data) => {
-			getCount();
-			return (
-				<span className='flex flex-row items-center gap-2'>
-					Counter incremented!{" "}
-					<a href={`https://suiscan.xyz/testnet/tx/${data.digest}`} target='_blank'>
-						<ExternalLink width={12} />
-					</a>
-				</span>
-			);
-		},
-		error: (error) => {
-			return error.message;
-		},
-	});
 }
